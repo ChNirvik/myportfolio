5984711d061d29c1b2294eab8ce131364da16b82
Nirvik Chakraborty <nirvikchakraborty@Nirviks-MacBook-Pro.local>
2026-02-28 15:58:32 +0530

    feat: populate actual Google Drive links for projects

--- src/app/api/work/route.ts
+++ src/app/api/work/route.ts
@@ -14,12 +14,72 @@ export async function GET() {
                 files: [
                     {
                         name: 'Brand Ad Link 1',
-                        path: 'https://drive.google.com/drive/folders/YOUR_LINK_HERE',
+                        path: 'https://drive.google.com/file/d/1VByP9feZEE77JRFs7L9UE2t-Af6mEssh/view?usp=sharing',
                         type: 'gdrive'
                     },
                     {
                         name: 'Brand Ad Link 2',
-                        path: 'https://drive.google.com/drive/folders/YOUR_LINK_HERE',
+                        path: 'https://drive.google.com/file/d/16i09F6k9vzvJbdRk08GEbf5aWGFdqjVP/view?usp=drive_link',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 3',
+                        path: 'https://drive.google.com/file/d/1uNlIXB8Kvuf_xIgOZAbFry1R3nUYGaa3/view?usp=drive_link',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 4',
+                        path: 'https://drive.google.com/file/d/16_F49w-Ci_G_PXjhAc3HvgKa1biqO9m6/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 5',
+                        path: 'https://drive.google.com/file/d/1EJ9eePAp5WcCbnD33yXWGgCU3UYM8H-b/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 6',
+                        path: 'https://drive.google.com/file/d/1Nb-EO9YYw1ejGk959bqHSiwwym5WjxI0/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 7',
+                        path: 'https://drive.google.com/file/d/1PwxVjkbZeD6B1d34z8BykZ0dzQ1tW6q1/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 8',
+                        path: 'https://drive.google.com/file/d/1Ivgzk17OfEXdkhyKz45RkfNovigBfJdP/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 9',
+                        path: 'https://drive.google.com/file/d/1bJlpYUuIkvHpuu3R6BIhI4a_VHmofHum/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 10',
+                        path: 'https://drive.google.com/file/d/14_Tq7fYpYs6LgK8oZQN3QxWqh5V0n_C0/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 11',
+                        path: 'https://drive.google.com/file/d/1sp1wAzNbgRhTj7IPcWjTQq3YJcXFOZfA/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 12',
+                        path: 'https://drive.google.com/file/d/1uNlIXB8Kvuf_xIgOZAbFry1R3nUYGaa3/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 13',
+                        path: 'https://drive.google.com/file/d/1XVLe-BD29Q5dJG0P0nFSv-MtH5dy6Jn6/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Brand Ad Link 14',
+                        path: 'https://drive.google.com/file/d/1syRlQW2xpI7JPbKURc5SIoHqaNc7I_wV/view?usp=sharing',
                         type: 'gdrive'
                     }
                 ]
@@ -31,21 +91,121 @@ export async function GET() {
                 thumbnail: '',
                 files: [
                     {
-                        name: 'Brand Collateral Folder',
-                        path: 'https://drive.google.com/drive/folders/YOUR_LINK_HERE',
+                        name: 'PhonePe SandBox Project',
+                        path: 'https://docs.google.com/presentation/d/1qQpL_erNgam9pBWU34gHV7j3pipXQEab/edit?usp=sharing&ouid=117951035039527866531&rtpof=true&sd=true',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Bira 91 Sales Deck',
+                        path: 'https://drive.google.com/file/d/15US71DQDav-vhAfy0YE60AzbaOZD1As9/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Explorex Tri-fold Brochure',
+                        path: 'https://drive.google.com/file/d/1dy4bFVDmykW1cATMSLxKhuC-AmdaPFj3/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Explorex Sales Deck',
+                        path: 'https://drive.google.com/file/d/15tQKrIsIEKClKQpvXctSVuXM8NRw_5cf/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Anupam Kher Kids Workshop Banner',
+                        path: 'https://drive.google.com/file/d/1GrIov7k55kW8uLEemWIiRL3dqsXM6ORU/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'AI Generated Brand Poster',
+                        path: 'https://drive.google.com/file/d/1dKnE2X2G3Lr5AtkR8to113Zy9usG9x0B/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Explorex Sales Deck',
+                        path: 'https://drive.google.com/file/d/1uQIw4KTxBGiFV4U2JoCbiqhaVXXRnoZi/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Explorex Tri-fold Brochure',
+                        path: 'https://drive.google.com/file/d/1MdRQ4YnVOeVWXgLJfmzp0nikJZmB1Gup/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'Actor Prepares Website Banner',
+                        path: 'https://drive.google.com/file/d/1cO6Aw5OrYEOHbmaED9DaUsHIWz2G6vhU/view?usp=sharing',
                         type: 'gdrive'
                     }
                 ]
             },
             {
                 id: 'on-ground-events',
-                title: 'On Ground Events',
+                title: 'Events',
                 category: 'Event Production',
                 thumbnail: '',
                 files: [
                     {
-                        name: 'Event Photos & Videos',
-                        path: 'https://drive.google.com/drive/folders/YOUR_LINK_HERE',
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1PHTkK7cVc33b2exVVIETKcpjH2pByez1/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1M0qKbxMDTIVfLejsr3C3ayh7uPi7yPEW/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1VEbc5kq4nNMf_AMdspLJhyMeyOnsiP16/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1XGkWwOlPOoxubTbKimo5Qco6mgBBX1L8/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1L4L3t0Klrv8u0Sr0btv3x8WMVfds8lfc/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1VeFzmSGPcT4fd2JrY_vMOLeMkmmNRjEo/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1grc7jcC01BHdN2-ObO98qTUhI1SI9LRP/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1lKa8SrcGiubQWb0GhZLwGCvKC81a3dq_/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1ZW5ue0-dOt_vAJUaRPAGKnaEDuv8r5BQ/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'TechSparks 2023',
+                        path: 'https://drive.google.com/file/d/1tzzhag_9M8ddJVmlNPN__EstJIqdC6LO/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'WOH Title Sponsorship',
+                        path: 'https://drive.google.com/file/d/17pHrYKFIpWe3VTu3DrjP1WPwpaPBsds9/view?usp=sharing',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'WOH Title Sponsorship',
+                        path: 'https://www.instagram.com/p/Ctg_MqpJXIr/?utm_source=ig_web_copy_link&igsh=MzRlODBiNWFlZA==',
+                        type: 'gdrive'
+                    },
+                    {
+                        name: 'WOH 2022',
+                        path: 'https://drive.google.com/file/d/1rLnlm13hzYUISNe4Tj7IfHDAUUe0uAuv/view?usp=sharing',
                         type: 'gdrive'
                     }
                 ]
@@ -58,7 +218,7 @@ export async function GET() {
                 files: [
                     {
                         name: 'PR Press Coverage',
-                        path: 'https://drive.google.com/drive/folders/YOUR_LINK_HERE',
+                        path: 'https://drive.google.com/drive/folders/15ZWSSvs2uHdiFrJqeC4t9Ujf5sLktG4Z?usp=sharing',
                         type: 'gdrive'
                     }
                 ]
